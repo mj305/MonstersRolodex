2061c8b2a4934c46035ca3675c5066c025c80b1f
mj305 <maria.j.beckles@gmail.com>
2020-07-05 18:30:08 -0400

    moved cards to cardlist componentnt

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -4,6 +4,10 @@ import './card-list.styles.css';
 
 export const CardList = (props) => {
   return (
-    <div className="card-list" > { props.children } </div>
+    <div className="card-list" > 
+        {props.monsters.map(monster => (
+              <h1 key={monster.id} > {monster.name} </h1>
+            ))}
+    </div>
   )
 }
