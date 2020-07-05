2b2697924d73dba87f0f4e7562b07d72654e7017
mj305 <maria.j.beckles@gmail.com>
2020-07-04 21:24:47 -0400

    fetched name from api and created grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.component.js'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-       {/*  <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Monsters Rolodex Coming Soon...
-        </p>
-        <a
-          className="App-link"
-          href="https://mariabeckles.herokuapp.com/"
-          target="_blank"
-        >
-          Created by Maria Beckles, 2020
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+   componentDidMount() {
+     fetch('https://jsonplaceholder.typicode.com/users')
+      .then( response => response.json() )
+      .then( users => this.setState({ monsters: users }) );
+        }
+
+  
+  render () {
+    return (
+      <div className='App' >
+         <CardList name='Maria' >
+            {this.state.monsters.map(monster => (
+              <h1 key={monster.id} > {monster.name} </h1>
+            ))}
+        </CardList> 
+      </div>
+    )
+  }
 }
 
 export default App;
+
