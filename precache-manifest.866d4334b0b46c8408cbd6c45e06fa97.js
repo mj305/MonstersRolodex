901e978ea4a4675e5c479e4b6a2b4c14a3c0ad58
mj305 <maria.j.beckles@gmail.com>
2020-07-21 20:24:12 -0400

    Updates

---
+++ precache-manifest.866d4334b0b46c8408cbd6c45e06fa97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed54f990ce4357c1e84756e46a12ab74",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "ab1a01532772ece4f715",
+    "url": "/MonstersRolodex/static/css/main.0a9636dd.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab1a01532772ece4f715",
+    "url": "/MonstersRolodex/static/js/main.61d1af58.chunk.js"
+  },
+  {
+    "revision": "0356c89559ba280291dc",
+    "url": "/MonstersRolodex/static/js/runtime-main.ee30fdb1.js"
+  }
+]);
